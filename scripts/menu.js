4021ab684c9e60a7abac8c152aa493bffb0839e3
Walocial <Walocial@hotmail.com>
2023-05-16 10:40:27 +0200

    Funktionel mobil menu og begyndt animation
    Jeg har fikset Innos Tools mangel på en responsive header - og dermed også manglen på en mobilvenlig navigation. Funktionaliteten er på plads, men det endelige design er ikke implementeret endnu. Yderligere, har jeg begyndt på animationen af landing pagen, samt tilføjet et blåligt billede filter.

---
+++ scripts/menu.js
@@ -0,0 +1,9 @@
+const mobilMenu = document.getElementById("mobilMenu");
+
+function showMenu(){
+    mobilMenu.style.right = "0";
+}
+function hideMenu(){
+    mobilMenu.style.right = "-100vw";
+}
+
