2983d087d462b83f1435bf9958a18da51737aaf9
Walocial <Walocial@hotmail.com>
2023-05-18 02:27:25 +0200

    Opdatering af animation og skip knap
    Tilføjet en knap magen til deres eksisterende knap. En usynlig skip til content knap er trods alt en god ide for at forbedre sidens betjening.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,9 @@
+//Vis mobil menu
 const mobilMenu = document.getElementById("mobilMenu");
 
-function showMenu(){
+function showMenu(){ //Sætter mobil menu's højre position til 0 (viser menuen)
     mobilMenu.style.right = "0";
 }
-function hideMenu(){
+function hideMenu(){ //Sætter mobil menu's højre position til -100vw (skjuler menuen)
     mobilMenu.style.right = "-100vw";
 }
-
