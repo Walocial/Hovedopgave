f821de5141fc9a6cf019196220e2770fde286705
Walocial <Walocial@hotmail.com>
2023-05-18 10:46:28 +0200

    Redesignet footer
    Jeg har redesignet Innos Tools' footer, men beholdt det samme indhold. Opsætningsmæssigt er det meget ens med deres eksisterende design, dog har jeg ændret baggrunden, optimeret den responsivt så den er sat op anderledes på henholdsvis mobil, tablet og pc enheder og derudover optimeret den i forhold til både tilgængelighed og semantisk opsætning.

--- scripts/slider.js
+++ scripts/slider.js
@@ -7,7 +7,10 @@ let timer = 5000;
 //Baggrundsbilleder
 billeder[0] = "url(./images/bilBck.jpg";
 billeder[1] = "url(./images/shirtBck.jpg";
-billeder[2] = "url(./images/teamet.jpg";
+billeder[2] = "url(./images/measureBck.jpg";
+billeder[3] = "url(./images/bil2Bck.png";
+billeder[4] = "url(./images/lumberBck.jpg";
+
 
 //Automatisk billed-slider
 function billedSlider(){
