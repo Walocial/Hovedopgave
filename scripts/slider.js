bbc8046724b26bee26f372bae279429386050574
Walocial <Walocial@hotmail.com>
2023-05-22 02:16:56 +0200

    Jsutering af farver og redesign af elementer
    I denne opdatering har jeg optimeret farverne, så de er variabler, samt optimeret billeder ved at lave dem til webp format. Sektionen omkring deres ydelser - kategorier - har jeg redesignet og optimeret efter tilgængelighed.

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,11 +5,11 @@ let billeder = [];
 let timer = 5000;
 
 //Baggrundsbilleder
-billeder[0] = "url(./images/bilBck.jpg";
-billeder[1] = "url(./images/shirtBck.jpg";
-billeder[2] = "url(./images/measureBck.jpg";
-billeder[3] = "url(./images/bil2Bck.png";
-billeder[4] = "url(./images/lumberBck.jpg";
+billeder[0] = "url(./images/bilBck.webp";
+billeder[1] = "url(./images/kneelitBck.webp";
+billeder[2] = "url(./images/shirtBck.webp";
+billeder[3] = "url(./images/bil2Bck.webp";
+billeder[4] = "url(./images/lumberBck.webp";
 
 
 //Automatisk billed-slider
